94c4149c1b6faa40a88743db7006cea3d8ba36e6
Sergio Jimenez <sgjimenezv@users.noreply.github.com>
2015-11-19 20:07:47 -0500

    adicionado calculo de pmi

--- dictionary_makers/pmi_v01.py
+++ dictionary_makers/pmi_v01.py
@@ -1,4 +1,6 @@
 import pickle
+import math
+
 PATH="diccionarios/"
 
 codigo_ISO="es"
@@ -16,3 +18,19 @@ lista_bigramas_ordenados=dic_bigramas.keys()
 lista_bigramas_ordenados.sort(key=lambda x:-dic_bigramas[x])
 for bigrama in lista_bigramas_ordenados[:20]: 
     print bigrama, dic_bigramas[bigrama]
+    
+dic_pmi={}
+for bigrama in dic_bigramas:
+    palabra1=bigrama[:bigrama.find("_")]
+    palabra2=bigrama[bigrama.find("_")+1:]
+    P_palabra1=float(dic_palabras[palabra1])/numero_palabras_corpus
+    P_palabra2=float(dic_palabras[palabra2])/numero_palabras_corpus
+    P_palabra1_seguida_palabra2=float(dic_bigramas[bigrama])/(numero_palabras_corpus-1)
+    pmi=math.log(P_palabra1_seguida_palabra2/(P_palabra1*P_palabra2))
+    dic_pmi[bigrama]=pmi
+
+lista_bigramas_ordenados=dic_pmi.keys()
+lista_bigramas_ordenados.sort(key=lambda x:-dic_pmi[x])
+for bigrama in lista_bigramas_ordenados[:20]: 
+    print bigrama, dic_pmi[bigrama]
+    
