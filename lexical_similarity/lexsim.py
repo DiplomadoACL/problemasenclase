701c1c3a23dc416eb8e7d94ee662083e10f88fae
Sergio Jimenez <sgjimenezv@users.noreply.github.com>
2015-12-01 18:56:34 -0500

    Update lexsim.py

--- lexical_similarity/lexsim.py
+++ lexical_similarity/lexsim.py
@@ -62,7 +62,29 @@ marca_de_tiempo=time.time()
 for articulo in rufino.get_articles(url):
     contador_articulos+=1
     if contador_articulos%reportar_cada_x_articulos==0:
-        print codigo_ISO+"{0} articulos procesados, {1} palabras procesadas, en {2} segundos".format(contador_articulos,contador_palabras,round(time.time()-marca_de_tiempo,1))
+        #***
+        predicciones=[]
+        for i in range(len(lista_pares_palabras)):
+            palabra1=lista_pares_palabras[i][0]
+            palabra2=lista_pares_palabras[i][1]
+            #palabra1,palabra2=lista_pares_palabras[i]  # equivalente a las dos lineas anteriores
+            P_palabra1=float(dic_freq_palabras[palabra1])/contador_oraciones
+            P_palabra2=float(dic_freq_palabras[palabra2])/contador_oraciones
+            P_p1yp2=float(dic_asoc_palabras[(palabra1,palabra2)])/contador_oraciones
+            if P_palabra1*P_palabra2==0 or P_p1yp2==0:
+                pmi=0.0
+            else:
+                pmi=math.log(P_p1yp2/(P_palabra1*P_palabra2))
+            print palabra1,palabra2,pmi,gold_standard[i]
+            predicciones.append(pmi)        
+        #***
+        print "{0}, {1} art., {2} palabras, r={3}, rho={4}, en {5} segundos".format(codigo_ISO, #0
+        contador_articulos, #1
+        contador_palabras,  #2
+        pearsonr(predicciones,gold_standard), #3
+        spearmanr(predicciones,gold_standard), #4
+        round(time.time()-marca_de_tiempo, #5
+        1))
         marca_de_tiempo=time.time()
     texto=rufino.clean_article(articulo).lower()
     oraciones=rufino.split_sentences(texto)
@@ -72,38 +94,12 @@ for articulo in rufino.get_articles(url):
         for palabra_dataset in lista_palabras_vocabulario_dataset:
             if palabra_dataset.lower() in palabras_oracion:
                 dic_freq_palabras[palabra_dataset]+=1
-        
         for palabra1,palabra2 in lista_pares_palabras:
             if (palabra1.lower() in palabras_oracion) and (palabra2.lower() in palabras_oracion):
                 dic_asoc_palabras[(palabra1,palabra2)]+=1
-
         contador_palabras=contador_palabras+len(palabras_oracion)
     if contador_palabras>tamano_corpus:
         break
-    
-# calcula pmi entre las palabras del dataset
-predicciones=[]
-
-#for palabra1,palabra2 in lista_pares_palabras:
-for i in range(len(lista_pares_palabras)):
-    palabra1=lista_pares_palabras[i][0]
-    palabra2=lista_pares_palabras[i][1]
-    #palabra1,palabra2=lista_pares_palabras[i]  # equivalente a las dos lineas anteriores
-    P_palabra1=float(dic_freq_palabras[palabra1])/contador_oraciones
-    P_palabra2=float(dic_freq_palabras[palabra2])/contador_oraciones
-    P_p1yp2=float(dic_asoc_palabras[(palabra1,palabra2)])/contador_oraciones
-    if P_palabra1*P_palabra2==0 or P_p1yp2==0:
-        pmi=0.0
-    else:
-        pmi=math.log(P_p1yp2/(P_palabra1*P_palabra2))
-    print palabra1,palabra2,pmi,gold_standard[i]
-    predicciones.append(pmi)
-
-
-print "numero de pares en el conjunto de datos:",len(lista_pares_palabras)
-print "tamano del vocabulario",len(dic_freq_palabras)
-print "Pearson correlation r=",pearsonr(predicciones,gold_standard)
-print "Spearman correlation rho=",spearmanr(predicciones,gold_standard)
 
 
         
